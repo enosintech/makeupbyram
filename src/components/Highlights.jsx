657ba8fae62be898058ed6cdab58a890733863b9
enosintech <enos.nsambaj@gmail.com>
2024-06-20 00:34:00 +0200

    hope

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -49,8 +49,8 @@ const Highlights = ({ scrollPosition }) => {
     }, [])
 
   return (
-    <section className="w-full h-[300vh] bg-white relative z-20 text-white">
-        <div ref={highlight} className="w-full h-[33.34%] overflow-hidden highlightPin relative">
+    <section className="w-full h-[300svh] bg-white relative z-20 text-white">
+        <div ref={highlight} className="w-full h-[100svh] overflow-hidden highlightPin relative">
             <div className="absolute top-4 sm:top-3 left-3 sm:left-5 flex flex-row-reverse items-center gap-x-2 z-10">
                 <Lottie animationData={scrollDown} className="size-8 sm:size-10" />
                 <p className="font-nohemiMedium sm:text-xl">SCROLL DOWN</p>
