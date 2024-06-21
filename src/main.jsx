f6adf241db99cf7c5ac1d1a1a812a4da26497f9a
enosintech <enos.nsambaj@gmail.com>
2024-06-21 14:31:02 +0200

    test againn

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import SmoothScroll from "./components/SmoothScroll";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SmoothScroll>
+      <App />
+    </SmoothScroll>
   </React.StrictMode>,
 )
