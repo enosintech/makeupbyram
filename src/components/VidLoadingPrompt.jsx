51a97eaa0095d513a545a8a74643fbd74fdf8ac1
enosintech <enos.nsambaj@gmail.com>
2024-06-22 10:12:30 +0200

    push

--- src/components/VidLoadingPrompt.jsx
+++ src/components/VidLoadingPrompt.jsx
@@ -2,8 +2,7 @@ const VidLoadingPrompt = () => {
   return (
     <div className="flex flex-col items-center text-center gap-y-2 h-fit">
         <span className="vidLoader"></span>
-        <p className="mt-10 font-black tracking-tight text-white text-[14px] sm:text-[16px]">LOADING VIDEO</p>
-        <p className="text-white font-light tracking-tight text-[8px] sm:text-[12px]">(TURN OFF LOW POWER MODE)</p>
+        <p className="text-white font-light tracking-tight text-[8px] sm:text-[12px]">(IF ON, TURN OFF LOW POWER MODE)</p>
     </div>
   )
 }
