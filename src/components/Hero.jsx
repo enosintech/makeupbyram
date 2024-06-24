6d4cc8f2fa6ae7c92d9c6d150c12b98558fde0fe
enosintech <enos.nsambaj@gmail.com>
2024-06-24 20:09:24 +0200

    testing final i think

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
     }, [])
 
   return (
-    <section className="w-full h-[100lvh] homescreen overflow-x-hidden bg-white">
+    <section style={{height: window.innerHeight}} className="w-full homescreen overflow-x-hidden bg-white">
         <div ref={hero} className="w-full h-full heroPin flex items-center justify-center">
             <VideoBackground />
             <div className="w-full h-full pt-[52px] sm:pt-[100px] md:pt-[130px] lg:pt-[150px] xl:pt-[170px] 2xl:pt-[210px]">
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -51,7 +51,7 @@ const Highlights = ({ scrollPosition }) => {
     }, [])
 
   return (
-    <div className="overflow-x-hidden">
+    <div style={{height: window.innerHeight}} className="overflow-x-hidden">
         <div ref={slider} className="md:w-[200vw] w-[400vw] h-[100lvh] bg-white text-white md:min-w-[200vw] min-w-[400vw] flex gap-x-1 py-1 moveLeft relative z-50">
                 <div className="w-[100vw] h-full absolute top-0 left-0 z-10">
                 <div className="absolute top-4 sm:top-3 left-3 sm:left-5 flex flex-row-reverse items-center gap-x-2 z-10">
